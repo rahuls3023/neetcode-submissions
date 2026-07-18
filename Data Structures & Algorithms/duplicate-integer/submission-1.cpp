1c2b8a85c4a8709f8aee4b95b2ef141ad8898d49
Rahul Singh <rahulsinghcode07@gmail.com>
2026-07-18 14:18:02 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+
+        for(int i=1;i<n;i++){
+            if(nums[i-1]==nums[i]) return true;
+        }
+        return false;
+    }
+};
