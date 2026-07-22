a87c47d641cf2ca744e3340b1903f46661f49ca3
Rahul Singh <rahulsinghcode07@gmail.com>
2026-07-22 10:32:39 +0530

    Add: plus-one - submission-0

---
+++ Data Structures & Algorithms/plus-one/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n=digits.size();
+        reverse(digits.begin(),digits.end());
+
+        int carry=1;
+
+        for(int i=0;i<digits.size();i++){
+            int sum=digits[i]+carry;
+
+            digits[i]=sum%10;
+            carry=sum/10;
+            if(carry==0) break;
+        }
+        if(carry>0){
+            digits.push_back(carry);
+        }
+        reverse(digits.begin(),digits.end());
+        return digits;
+    }
+};
