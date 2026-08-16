ebf32fbc78c34f016dbb1e8ed81236e961c38faa
Rahul Singh <rahulsinghcode07@gmail.com>
2026-08-16 07:40:58 +0530

    Add: number-of-senior-citizens - submission-0

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count=0;
+
+        for(int i=0;i<details.size();i++){
+            string current=details[i];
+
+            int age=(current[11]-'0')*10+(current[12]-'0');
+
+            if(age>60) count++;
+        }
+        return count;
+    }
+};
