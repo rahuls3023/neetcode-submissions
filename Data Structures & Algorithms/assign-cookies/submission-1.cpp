6fda9b7e9b38ec2bba068ea4ded7547a49bc0ed5
Rahul Singh <rahulsinghcode07@gmail.com>
2026-08-19 06:55:23 +0530

    Add: assign-cookies - submission-1

---
+++ Data Structures & Algorithms/assign-cookies/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(begin(g),end(g));
+        sort(begin(s),end(s));
+        int i=0;
+        int j=0;
+        int count=0;
+
+        while(i<g.size() && j<s.size()){
+            if(g[i]<=s[j]){
+                count++;
+                i++;
+                j++;
+            }
+            else{
+                //cant satisfy current children with this cookie , move to next cookie
+                j++;
+
+            }
+        }
+        return count;
+    }
+};
