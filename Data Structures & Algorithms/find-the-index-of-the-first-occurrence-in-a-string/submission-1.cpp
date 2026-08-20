28f5806c3f3121f4d26ad43dc6679e7c73988eed
Rahul Singh <rahulsinghcode07@gmail.com>
2026-08-20 09:47:37 +0530

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-1

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public://optimal-> 2 pointer
+    int strStr(string haystack, string needle) {
+        int m=haystack.size();
+        int n=needle.size();
+
+        for(int i=0;i<=(m-n);i++){
+            int j=0;
+
+            while(j<n && haystack[i+j]==needle[j]){
+                j++;
+            }
+            if(j==n) return i;
+        }
+        return -1;
+    }
+};
