2c1a7b1340ae1023adb9911b763764457b3c97f8
Rahul Singh <rahulsinghcode07@gmail.com>
2026-08-25 09:15:31 +0530

    Add: count-primes - submission-1

---
+++ Data Structures & Algorithms/count-primes/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public://approach-1> O(N^2)
+    bool checkPrime(int num){
+        int cnt=0;
+
+        for(int i=1;i<=num;i++){
+            if(num%i==0) cnt++;
+        }
+        if(cnt==2) return true;
+        else return false;
+    }
+    int countPrimes(int n) {
+        int count=0;
+
+        for(int i=2;i<n;i++){
+            if(checkPrime(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+};
