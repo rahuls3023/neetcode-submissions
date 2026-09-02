8db8f94cd8951c9556ea1b9dd096a1961cf638be
Rahul Singh <rahulsinghcode07@gmail.com>
2026-09-02 06:45:23 +0530

    Add: subarray-sum-equals-k - submission-0

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public://brute force
+    int subarraySum(vector<int>& nums, int k) {
+        int total=0;
+
+        for(int i=0;i<nums.size();i++){
+            int sum=0;
+            for(int j=i;j<nums.size();j++){
+                sum+=nums[j];
+
+                if(sum==k) total++;
+            }
+        }
+        return total;
+    }
+};
