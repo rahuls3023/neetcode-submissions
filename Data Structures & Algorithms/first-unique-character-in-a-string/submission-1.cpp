d9fafceeb0218513c0652a852cd864d4a063880f
Rahul Singh <rahulsinghcode07@gmail.com>
2026-09-02 06:20:38 +0530

    Add: first-unique-character-in-a-string - submission-1

---
+++ Data Structures & Algorithms/first-unique-character-in-a-string/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public://approach-1> sorting
+    int firstUniqChar(string s) {
+        vector<pair<char,int>>V;
+
+        //store original indices
+        for(int i=0;i<s.size();i++){
+            V.push_back({s[i],i});
+        }
+        sort(begin(V),end(V));
+
+        int ans=s.size();
+
+        for(int i=0;i<V.size();i++){
+           // unique character
+            if ((i == 0 || V[i].first != V[i - 1].first) &&
+                (i == V.size() - 1 || V[i].first != V[i + 1].first)) {
+                
+                ans = min(ans, V[i].second);
+            }
+        }
+        if(ans==s.size()) return -1;
+        else return ans;
+    }
+};
