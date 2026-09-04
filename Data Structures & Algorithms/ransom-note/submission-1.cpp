ff351a02e6c2a2d9e865eb7bdf53604afaf8ae07
Rahul Singh <rahulsinghcode07@gmail.com>
2026-09-04 09:44:21 +0530

    Add: ransom-note - submission-1

---
+++ Data Structures & Algorithms/ransom-note/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        vector<int>freq(26,0);
+
+        for(auto it:ransomNote){
+            freq[it-'a']++;
+        }
+
+        for(int i=0;i<magazine.size();i++){
+            if( freq[magazine[i]-'a']>0){
+                 freq[magazine[i]-'a']--;
+            }
+        }
+
+        for(int i=0;i<26;i++){
+            if(freq[i]>0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
